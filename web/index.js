b0c236d3ae98a63e91c93cfbb3ba8ccc8f218935
Samuel Machado <samuelmachado1@gmail.com>
2022-06-11 18:02:36 -0300

    [dev] Implementation of the player's lateral movements and adjustments to the vertical movement.

--- web/index.js
+++ web/index.js
@@ -4,16 +4,16 @@ const myContext = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-const gravity = 4.5;
+const gravity = 5;
 class Player {
   constructor() {
     this.positions = {
       x: 100,
-      y: 100
+      y: 150
     }
     this.velocity = {
       x: 0,
-      y: 1
+      y: 0
     }
     this.width = 30;
     this.height = 30;
@@ -24,7 +24,9 @@ class Player {
   }
   update() {
     this.draw();
+    this.positions.x += this.velocity.x;
     this.positions.y += this.velocity.y;
+
     if (this.positions.y + this.height + this.velocity.y <= canvas.height) {
       this.positions.y += gravity;
     } else
@@ -33,12 +35,62 @@ class Player {
 }
 
 const player = new Player();
-player.update();
-
-function animated() {
-  requestAnimationFrame(animated);
-  myContext.clearRect(0, 0, innerWidth, innerHeight);
-  player.update();
+const keys = {
+  right: {
+    pressed: false,
+  },
+  left: {
+    pressed: false,
+  },
 }
 
-animated();
+function animate() {
+  requestAnimationFrame(animate);
+  myContext.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
+
+  if (keys.right.pressed) {
+    player.velocity.x = 3;
+  } else if (keys.left.pressed) {
+    player.velocity.x = -3;
+  } else player.velocity.x = 0;
+}
+
+animate();
+
+addEventListener('keydown', ({ key }) => {
+  switch (key) {
+    case 'ArrowLeft':
+      console.log('left');
+      keys.left.pressed = true;
+      break;
+    case 'ArrowRight':
+      console.log('Right');
+      keys.right.pressed = true;
+      break;
+    case 'ArrowUp':
+      player.positions.y -= 200;
+      player.positions.y += gravity;
+      break;
+
+  }
+})
+addEventListener('keyup', ({ key }) => {
+
+  switch (key) {
+    case 'ArrowLeft':
+      console.log('left');
+      keys.left.pressed = false;
+      break;
+    case 'ArrowRight':
+      console.log('Right');
+      keys.right.pressed = false;
+      break;
+    case 'ArrowUp':
+      // player.positions.y -= 120;
+      // player.positions.y += gravity;
+      break;
+
+  }
+})
+

11e9fc2f66984011c03907ad73d36ce029c95393
Samuel Machado <samuelmachado1@gmail.com>
2022-06-11 12:55:50 -0300

    [dev] Gravity implementing and layout adjusts

--- web/index.js
+++ web/index.js
@@ -1,5 +1,44 @@
 const canvas = document.querySelector('canvas');
-
 const myContext = canvas.getContext('2d');
 
-console.log('myContext', myContext);
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+const gravity = 4.5;
+class Player {
+  constructor() {
+    this.positions = {
+      x: 100,
+      y: 100
+    }
+    this.velocity = {
+      x: 0,
+      y: 1
+    }
+    this.width = 30;
+    this.height = 30;
+  }
+  draw() {
+    myContext.fillStyle = 'red';
+    myContext.fillRect(this.positions.x, this.positions.y, this.width, this.height);
+  }
+  update() {
+    this.draw();
+    this.positions.y += this.velocity.y;
+    if (this.positions.y + this.height + this.velocity.y <= canvas.height) {
+      this.positions.y += gravity;
+    } else
+      this.velocity.y = 0;
+  }
+}
+
+const player = new Player();
+player.update();
+
+function animated() {
+  requestAnimationFrame(animated);
+  myContext.clearRect(0, 0, innerWidth, innerHeight);
+  player.update();
+}
+
+animated();
